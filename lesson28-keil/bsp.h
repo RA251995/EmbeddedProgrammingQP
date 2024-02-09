d0889337d1f1acb400e32d3e66d0e24ae0c5a6c8
RA251995 <rijoabraham251995@gmail.com>
2024-02-09 09:28:13 +0530

    Add lesson 28 Keil IDE code

---
+++ lesson28-keil/bsp.h
@@ -0,0 +1,22 @@
+#ifndef __BSP_H
+#define __BSP_H
+
+#include <stdint.h>
+
+#define BSP_TICKS_PER_SEC   (1000U)
+
+extern QXSemaphore SW1_sem;
+
+void BSP_init(void);
+void BSP_ledGreenOn(void);
+void BSP_ledGreenOff(void);
+void BSP_ledGreenToggle(void);
+void BSP_ledYellowOn(void);
+void BSP_ledYellowOff(void);
+void BSP_ledYellowToggle(void);
+void BSP_ledOrangeOn(void);
+void BSP_ledOrangeOff(void);
+void BSP_ledOrangeToggle(void);
+void BSP_sendMorseCode(uint32_t bitmask);
+
+#endif /* __BSP_H */
